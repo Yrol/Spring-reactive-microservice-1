2c821115a7aaa485fa2c0cf33df95d2e3b0f78c9
Yrol <yrollgayanth@yahoo.com>
2023-11-22 20:30:40 +1100

    Adding the movies-info service

--- movies-info-sevice/src/main/java/blog/yrol/MovieInfoServiceApplication.java
+++ movies-info-sevice/src/main/java/blog/yrol/MovieInfoServiceApplication.java
@@ -5,19 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "blog.yrol")
-public class MovieServiceApplication implements CommandLineRunner {
-    public static final Logger LOG = LoggerFactory.getLogger(MovieServiceApplication.class);
+public class MovieInfoServiceApplication implements CommandLineRunner {
+
+    public static final Logger LOG = LoggerFactory.getLogger(MovieInfoServiceApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(MovieServiceApplication.class, args);
+        SpringApplication.run(MovieInfoServiceApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        LOG.info("MovieServiceApplication started..");
+        LOG.info("MovieInfoServiceApplication started..");
     }
 }
--- reactive-programming-using-reactor/src/main/java/blog/yrol/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/blog/yrol/FluxAndMonoGeneratorService.java
@@ -3,7 +3,6 @@ package blog.yrol;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -13,12 +12,8 @@ public class FluxAndMonoGeneratorService implements CommandLineRunner {
 
     public static final Logger LOG = LoggerFactory.getLogger(FluxAndMonoGeneratorService.class);
 
-    public static void main(String[] args) {
-        SpringApplication.run(FluxAndMonoGeneratorService.class, args);;
-    }
-
     @Override
     public void run(String... args) throws Exception {
-        LOG.info("ReactiveProgrammingApplication started..");
+        LOG.info("FluxAndMonoGeneratorService started..");
     }
 }
