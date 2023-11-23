afb6c0c2e477220c473b728b7115a7b107b2799c
Yrol <yrollgayanth@yahoo.com>
2023-11-23 21:29:34 +1100

    Dockerizing mongo db and add configs

--- movies-info-sevice/src/main/java/blog/yrol/MovieInfoServiceApplication.java
+++ movies-info-sevice/src/main/java/blog/yrol/MovieInfoServiceApplication.java
@@ -4,9 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class MovieInfoServiceApplication implements CommandLineRunner {
 
     public static final Logger LOG = LoggerFactory.getLogger(MovieInfoServiceApplication.class);
