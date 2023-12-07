a5ac9393f5222cbed8c2a2549efa074507745e73
Yrol <yrollgayanth@yahoo.com>
2023-12-07 19:33:34 +1100

    Adding movies domain and controller classes - movies-service ms

---
+++ movies-service/src/main/java/blog/yrol/controller/MoviesController.java
@@ -0,0 +1,11 @@
+package blog.yrol.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/v1/movies")
+public class MoviesController {
+
+}
