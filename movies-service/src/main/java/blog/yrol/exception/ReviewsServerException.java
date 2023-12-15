dbb5b907fb8c4b1bcadcf7c0566b18277a09c9e0
Yrol <yrollgayanth@yahoo.com>
2023-12-15 21:31:18 +1100

    Modifying MovieInfoService and MovieReviewService error handling and adding test cases

---
+++ movies-service/src/main/java/blog/yrol/exception/ReviewsServerException.java
@@ -0,0 +1,12 @@
+package blog.yrol.exception;
+
+public class ReviewsServerException extends RuntimeException {
+
+    private String message;
+
+
+    public ReviewsServerException(String message) {
+        super(message);
+        this.message = message;
+    }
+}
